45e0fdc62816807a6b91e649786612e9cc8c1f74
nglthu <nglthu@gmail.com>
2016-10-11 13:28:16 +1300

    binding function

--- test.js
+++ test.js
@@ -10,3 +10,19 @@ var rose=Object.create(gold);
 gold.z=2;
 console.log(rose.a);
 console.log(rose.z);
+console.log(rose.toString());
+
+//binding function
+var carlike=function(obj, loc){
+	obj.loc=loc;
+	
+	obj.move =  move;
+	return obj;
+}
+var move = function(){
+	this.loc ++;
+}
+
+var amy = carlike({},1);
+amy.move();
+console.log(amy.loc);
